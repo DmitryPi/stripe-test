bf6e128a27074121b1876b60e443a7b5d7e707b0
Dmitry G <sdprecovery@gmail.com>
2023-02-17 13:20:56 +0300

    Tests for service functions;

--- shop/payments/tests/test_services.py
+++ shop/payments/tests/test_services.py
@@ -1,8 +1,28 @@
+import json
+from decimal import Decimal
+
 import pytest
 
 from shop.products.tests.factories import Item, ItemFactory
 
-from ..services import convert_currency_symbol_to_stripe_type, convert_price_to_cents
+from ..services import (
+    convert_currency_symbol_to_stripe_type,
+    convert_price_by_currency,
+    convert_price_to_cents,
+    get_selected_currency,
+)
+
+
+def test_get_selected_currency():
+    body = json.dumps({"currency": Item.Currency.RUB})
+    result = get_selected_currency(body)
+    assert result == Item.Currency.RUB
+
+
+def test_get_selected_currency_blank():
+    body = json.dumps({})
+    result = get_selected_currency(body)
+    assert not result
 
 
 def test_convert_currency_symbol_to_stripe_type_rub():
@@ -15,6 +35,24 @@ def test_convert_currency_symbol_to_stripe_type_usd():
 
 def test_convert_currency_symbol_to_stripe_type_unknown():
     assert convert_currency_symbol_to_stripe_type("123") == "rub"
+
+
+def test_convert_price_by_currency_rub():
+    price = Decimal("1000.00")
+    result = convert_price_by_currency(Item.Currency.RUB, price)
+    assert result == price
+
+
+def test_convert_price_by_currency_usd():
+    price = Decimal("1000.00")
+    result = convert_price_by_currency(Item.Currency.USD, price)
+    assert result == price / 70
+
+
+def test_convert_price_by_currency_unknown():
+    price = Decimal("1000.00")
+    result = convert_price_by_currency("123", price)
+    assert result == price
 
 
 @pytest.mark.django_db
